485766bcfe4c095dd16dc08615fdcddf9ca05698
Noura Loudani <contact.nouraloudani@gmail.com>
2020-11-09 23:50:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
       }
     }
 
+	  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -81,7 +84,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash[first]
 end
 
 def changing_alan
@@ -125,5 +128,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    programmer_hash[]
 
 end
